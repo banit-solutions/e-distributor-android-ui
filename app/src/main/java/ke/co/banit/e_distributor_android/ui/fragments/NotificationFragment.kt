913edf84b2574d8703336e38e6836ea5f327e6b1
Angatia Benson <angatiabenson1@gmail.con>
2023-08-29 23:22:19 -0500

    feat: continue home page UI and impl notification UI

---
+++ app/src/main/java/ke/co/banit/e_distributor_android/ui/fragments/NotificationFragment.kt
@@ -0,0 +1,20 @@
+package ke.co.banit.e_distributor_android.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ke.co.banit.e_distributor_android.R
+
+class NotificationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
+
+}
